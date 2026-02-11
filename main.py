272cc21b1c00d98a5d51f230b30cb983f0fa9e1a
Mengxuan <123134831+Meng-xuanny@users.noreply.github.com>
2026-02-11 15:08:41 +1100

    changed cors

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="VisionSafe Road Risk API")
 # Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or frontend origin
+    allow_origins=["https://lemon-ground-053dab000.2.azurestaticapps.net/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
